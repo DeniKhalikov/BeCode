17fbbf6872a77f2ab2aab33a3480db08ddb60e10
Deni Khalikov <deni@Denis-MacBook-Pro.local>
2020-06-08 15:31:53 +0200

    adding files

--- JavaScript/01-base/04-asv-confirm/script.js
+++ JavaScript/01-base/04-asv-confirm/script.js
@@ -1,16 +1,18 @@
-/* becode/javascript
- *
- * /01-base/04-asv-confirm/script.js - 1.4: ASV avec confirmation
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
-(function() {
+(function question() {
 
-    // your code here
+    let age = prompt("how old are you?");
+    let gender = prompt("whats your gender?");
+    let town = prompt("what town are you from?");
 
+    if (confirm("so you are " + age + " years old " + gender + " and live in " + town))
+    {
+        alert("thank you for confirming")
+    }
+    else
+    {
+        question()
+    }
+    
 })();
