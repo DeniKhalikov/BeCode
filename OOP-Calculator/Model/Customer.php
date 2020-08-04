4f0fde3d9b100089fb1f2087a3ed879c6a33d3d6
Deni Khalikov <deni.khalikov@gmail.com>
2020-08-04 14:15:26 +0200

    changing stuff

--- OOP-Calculator/Model/Customer.php
+++ OOP-Calculator/Model/Customer.php
@@ -10,4 +10,85 @@ class Customer
      * @var Product[]
      */
     private array $products;
+
+    /**
+     * Customer constructor.
+     * @param int $id
+     * @param int $groupId
+     * @param int $fixedDiscount
+     * @param int $variableDiscount
+     * @param string $firstName
+     * @param string $lastName
+     */
+
+
+
+    public function __construct(int $id, int $groupId, int $fixedDiscount, int $variableDiscount, string $firstName, string $lastName)
+    {
+        $this->id = $id;
+        $this->groupId = $groupId;
+        $this->fixedDiscount = $fixedDiscount;
+        $this->variableDiscount = $variableDiscount;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getGroupId(): int
+    {
+        return $this->groupId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getFixedDiscount(): int
+    {
+        return $this->fixedDiscount;
+    }
+
+    /**
+     * @return int
+     */
+    public function getVariableDiscount(): int
+    {
+        return $this->variableDiscount;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @return Product[]
+     */
+    public function getProducts(): array
+    {
+        return $this->products;
+    }
+
+
+
 }
--- OOP-Calculator/View/homepage.php
+++ OOP-Calculator/View/homepage.php
@@ -2,27 +2,26 @@
     <!-- this is the view, try to put only simple if's and loops here.
     Anything complex should be calculated in the model -->
     <section>
-        <p>This is home page</p>
         <form method="post">
             <div class="input-group mb-3">
                 <select name="productId" class="custom-select" >
                     <option selected disabled value="">Choose a product</option>
                     <?php
                     /**
-                     * @var Product[] $products
+                     * @var Product[] $customers
                      */
-                    foreach ($products as $product) {
-                        $name = ucwords($product->getName());
-                        $price = $product->getPrice()/100;
+                    foreach ($customers as $customer) {
+                        $name = ucwords($customer->getName());
+                        $price = $customer->getPrice()/100;
                         $selected = '';
                         if(isset($_POST['productId'])) {
                             $id = (int)htmlspecialchars(trim($_POST['productId']));
-                            if($product->getId() === $id) {
+                            if($customer->getId() === $id) {
                                 $selected = 'selected';
                             }
                         }
                         echo "
-                <option value='{$product->getId()}' {$selected}>{$name} | &euro;{$price}</option>
+                <option value='{$customer->getId()}' {$selected}>{$name} | &euro;{$price}</option>
                 ";
                     }
                     ?>
@@ -32,5 +31,35 @@
                 </div>
             </div>
         </form>
+        <form method="post">
+            <div class="input-group mb-3">
+                <select name="customerId" class="custom-select" >
+                    <option selected disabled value="">Choose a customer</option>
+                    <?php
+                    /**
+                     * @var Customer[] $customers
+                     */
+                    foreach ($customers as $customer) {
+                        // . " " . concatinating: two strings together
+                        $name = ucwords($customer->getFirstName() . " " . $customer->getLastName());
+                        $selected = '';
+                        if(isset($_POST['customerId'])) {
+                            $id = (int)htmlspecialchars(trim($_POST['customerId']));
+                            if($customer->getId() === $id) {
+                                $selected = 'selected';
+                            }
+                        }
+                        echo "
+                <option value='{$customer->getId()}' {$selected}>{$name}</option>
+                ";
+                    }
+                    ?>
+                </select>
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="submit">Submit</button>
+                </div>
+            </div>
+        </form>
+
     </section>
 <?php require 'includes/footer.php'?>
